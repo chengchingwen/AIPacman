03cb03eabebe5e649e6d721b30af7a13687fbbfd
chengchingwen <adgjl5645@hotmail.com>
2016-05-11 01:29:27 +0800

    finish learning q5

--- reinforcement/qlearningAgents.py
+++ reinforcement/qlearningAgents.py
@@ -105,8 +105,10 @@ class QLearningAgent(ReinforcementAgent):
         legalActions = self.getLegalActions(state)
         action = None
         "*** YOUR CODE HERE ***"
-        action = random.choice(legalActions)
-
+        if util.flipCoin(self.epsilon):
+            action = random.choice(legalActions)
+        else:
+            action = self.computeActionFromQValues(state)
         return action
 
     def update(self, state, action, nextState, reward):
